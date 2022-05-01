fbaf0f0305f75486a6bb408143b6abd699ab4d49
Rithik <duttrithik@gmail.com>
2022-05-01 08:36:11 +0530

    changed static layout

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+var app = express();
+
+app.use(express.static(path.resolve(__dirname)));
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+})
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log(`Server up on port ${port}`)
+})

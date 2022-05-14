79641260e3ed5a53cfa07664aee24880204076fd
Rahul Kumar <rahulkumar@197BLRMBT3019.local>
2022-05-14 18:08:25 +0530

    register button

--- register.php
+++ register.php
@@ -9,6 +9,8 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 </head>
 <body>
 
@@ -98,6 +100,7 @@
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
                   class="img-fluid" alt="Sample image">
 
+
               </div>
             </div>
           </div>
@@ -131,16 +134,73 @@ function makePayment(){
 
   if(team_size > 0 && team_size<4){
     var options = {
-    "key": "rzp_live_SNuGTvcFhUHMYy", // Enter the Key ID generated from the Dashboard
-    "amount": team_size * 25000, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    // "key": "rzp_test_nLYkSOe9jqHeVG",//(Hod) 
+    "key": "rzp_live_SNuGTvcFhUHMYy",//(Rahul) 
+
+    
+    // Enter the Key ID generated from the Dashboard
+
+    // rzp_live_SNuGTvcFhUHMYy(Rahul key id)
+    "amount": "100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     "currency": "INR",
     "name": "Hackman 5.0",
     "description": "",
     "image": "https://example.com/your_logo",
     //"order_id": "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
     "handler": function (response){
+
+
+     
+
+
       paymentid = response.razorpay_payment_id; 
+      console.log(team_name);
+      console.log(name);
+      console.log(email);
+      console.log(phone);
+      console.log(team_size);
+      console.log(paymentid);
+
         alert("Registration succesfull");
+
+
+
+
+        <?php
+if(!isset($_POST['submit']))
+{
+
+echo "error; you need to submit the form!";
+}
+$name = $_GET['name'];
+$visitor_email = $_GET['email'];
+
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+echo "Name and email are mandatory!";
+exit;
+}
+$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+ "Here is the message:\n".
+
+$to = "khan.koder@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//done. redirect to thank-you page.
+
+
+
+?> 
+
+
+
         //add the google sheets api
         window.location = "index.html";
     },
@@ -159,61 +219,12 @@ function makePayment(){
         // alert(response.error.code);
         alert(response.error.description);
         window.location = "register.php";
-        // alert(response.error.source);
-        // alert(response.error.step);
-        // alert(response.error.reason);
-        // alert(response.error.metadata.order_id);
-        // alert(response.error.metadata.payment_id);
 });
   }else{
     alert("The Team size varies from 1 to 3. Please adjust your team accordingly")
     window.location = "register.php";
   }
-
 }
-
-
-// var options = {
-//     "key": "rzp_live_SNuGTvcFhUHMYy", // Enter the Key ID generated from the Dashboard
-//     "amount": "100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-//     "currency": "INR",
-//     "name": "Hackman 5.0",
-//     "description": "Test Transaction",
-//     "image": "https://example.com/your_logo",
-//     //"order_id": "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-//     "handler": function (response){
-
-
-//         alert(response.razorpay_payment_id);
-//         alert(response.razorpay_order_id);
-//         alert(response.razorpay_signature)
-//     },
-//     "prefill": {
-//         "name": "Rahul Kumar",
-//         "email": "hello@gmail.com",
-//         "contact": "hat"
-//     },
-//     "notes": {
-//         "address": "Razorpay Corporate Office"
-//     },
-//     "theme": {
-//         "color": "#3399cc"
-//     }
-// };
-// var rzp1 = new Razorpay(options);
-// rzp1.on('payment.failed', function (response){
-//         alert(response.error.code);
-//         alert(response.error.description);
-//         alert(response.error.source);
-//         alert(response.error.step);
-//         alert(response.error.reason);
-//         alert(response.error.metadata.order_id);
-//         alert(response.error.metadata.payment_id);
-// });
-// document.getElementById('rzp-button1').onclick = function(e){
-//     rzp1.open();
-//     e.preventDefault();
-// }
 </script>
 
 </body>
